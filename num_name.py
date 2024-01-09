8c225234433dd6b2c9605103f21061caf482dbf2
Jacqueline Grimm <110108204+jacquelinegrimm@users.noreply.github.com>
2024-01-09 19:09:19 +0200

    Add files via upload

---
+++ num_name.py
@@ -0,0 +1,13 @@
+import os
+
+# Rename all photos in a folder numerically
+
+directory = "~/Desktop/Test"
+directory = os.path.expanduser(directory)
+counter = 1
+
+for filename in os.listdir(directory):
+    if filename.endswith(".png"):
+        new_filename = f"{counter:04d}.jpg"
+        os.rename(os.path.join(directory, filename), os.path.join(directory, new_filename))
+        counter += 1
---
+++ random_name.py
@@ -0,0 +1,28 @@
+import os
+import random
+import string
+
+# Rename all photos in a folder with a random name
+
+def generate_random_filename():
+    length = 6
+    characters = string.ascii_letters + string.digits
+    random_string = ''.join(random.choice(characters) for i in range(length))
+    return random_string + ".png"
+
+directory = "~/Desktop/Test/"
+directory = os.path.expanduser(directory)
+
+print(f"Looking in directory: {directory}")
+files = os.listdir(directory)
+
+for filename in files:
+    if filename.endswith(".png"):
+        new_filename = generate_random_filename()
+
+        # Ensure the new filename doesn't already exist
+        while os.path.exists(os.path.join(directory, new_filename)):
+            new_filename = generate_random_filename()
+
+        print(f"Renaming {filename} to {new_filename}")
+        os.rename(os.path.join(directory, filename), os.path.join(directory, new_filename))
