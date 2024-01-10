33f038e95fa9cb857c3153ea0bd79f00238c75f1
Jacqueline Grimm <110108204+jacquelinegrimm@users.noreply.github.com>
2024-01-10 07:03:21 +0200

    Update num_name.py

--- num_name.py
+++ num_name.py
@@ -8,6 +8,6 @@ counter = 1
 
 for filename in os.listdir(directory):
     if filename.endswith(".png"):
-        new_filename = f"{counter:04d}.jpg"
+        new_filename = f"{counter:04d}.png"
         os.rename(os.path.join(directory, filename), os.path.join(directory, new_filename))
         counter += 1
