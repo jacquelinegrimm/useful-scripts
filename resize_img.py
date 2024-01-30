1ea5d292f6a4c721419ebe385404055f82a1364a
Jacqueline Grimm <110108204+jacquelinegrimm@users.noreply.github.com>
2024-01-30 12:34:35 +0200

    Update resize_img.py

--- resize_img.py
+++ resize_img.py
@@ -1,15 +1,18 @@
 import os
 from PIL import Image
 
-directory = "~/Desktop/Test"  # Change this to your directory path
+# This script resizes all images in a folder by a certain percentage
+# Note that this will overwrite the originals!
+
+directory = "~/MyDirectory"  # Change this to your directory path
 directory = os.path.expanduser(directory)
 
 for filename in os.listdir(directory):
-    if filename.endswith(".jpg"):
+    if filename.endswith(".jpg"): # Change this to the applicable file extension
         file_path = os.path.join(directory, filename)
 
         with Image.open(file_path) as img:
-            # Calculate the new size (20% of the original size)
+            # Calculate the new size (e.g., 20% of the original size)
             new_width = int(img.width * 0.2)
             new_height = int(img.height * 0.2)
 
@@ -17,5 +20,4 @@ for filename in os.listdir(directory):
             resized_img = img.resize((new_width, new_height), Image.ANTIALIAS)
 
             # Save the resized image back to the directory
-            # Note that this will overwrite the original
             resized_img.save(file_path)
