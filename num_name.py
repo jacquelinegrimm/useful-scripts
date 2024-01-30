1080fd65451ea53c89ffd6e1aea9a6f07daf3afc
Jacqueline Grimm <110108204+jacquelinegrimm@users.noreply.github.com>
2024-01-30 12:27:41 +0200

    Update num_name.py

--- num_name.py
+++ num_name.py
@@ -1,13 +1,13 @@
 import os
 
-# Rename all photos in a folder numerically
+# Rename all images in a folder numerically
 
-directory = "~/Desktop/Test"
+directory = "~/MyDirectory" # Replace with the desired directory
 directory = os.path.expanduser(directory)
 counter = 1
 
 for filename in os.listdir(directory):
-    if filename.endswith(".png"):
+    if filename.endswith(".png"): # Replace with the appropriate file extension
         new_filename = f"{counter:04d}.png"
         os.rename(os.path.join(directory, filename), os.path.join(directory, new_filename))
         counter += 1
